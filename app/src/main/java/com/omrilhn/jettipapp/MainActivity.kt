c7f868815224cc82fa515bc9a65e5e06a504e0de
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-03 15:06:01 +0300

    TopHeader and MainContent outline

--- app/src/main/java/com/omrilhn/jettipapp/MainActivity.kt
+++ app/src/main/java/com/omrilhn/jettipapp/MainActivity.kt
@@ -3,44 +3,92 @@ package com.omrilhn.jettipapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.omrilhn.jettipapp.ui.theme.JetTipAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetTipAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                topHeader()
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit){
+    JetTipAppTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background){
+               content()
+        }
+    }
+}
+//@Preview
+@Composable
+fun topHeader(totalPerPerson : Double = 134.0)
+{//clip: For give a shape to the container
+    Surface(modifier = Modifier.fillMaxWidth()
+        .height(150.dp)
+        .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
+        ,color = Color(0xFFB160BF)
+    ){// .clip(shape = CircleShape.copy(all = CornerSize(12.dp) -> second way of giving shape to the corners
+        Column(modifier = Modifier.padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+            val total = "%.2f".format(totalPerPerson)
+            Text(text="Total Per Person",
+                style = MaterialTheme.typography.headlineMedium)
+            Text(text = "$$total",
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.ExtraBold)
+        }
+    }
+}
+@Preview
+@Composable
+fun MainContent()
+{
+    Surface(modifier = Modifier.padding(2.dp)
+        .fillMaxWidth(),shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp,color = Color.LightGray)){
+            Column()
+            {
+              
+            }
+
+    }
 }
 
-@Preview(showBackground = true)
+
+
+//@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetTipAppTheme {
-        Greeting("Android")
+        MyApp {
+            Text("Hello again!")
+        }
     }
 }

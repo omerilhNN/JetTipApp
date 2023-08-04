0888d76f8c3b3ea71b6b6abd142b5bc058e072cd
Ömer Faruk İlhan <omerilhan2452@gmail.com>
2023-08-04 11:58:10 +0300

    InputField fix and splitting bill amount buttons

--- app/src/main/java/com/omrilhn/jettipapp/MainActivity.kt
+++ app/src/main/java/com/omrilhn/jettipapp/MainActivity.kt
@@ -1,36 +1,51 @@
 package com.omrilhn.jettipapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.omrilhn.jettipapp.components.InputField
 import com.omrilhn.jettipapp.ui.theme.JetTipAppTheme
+import com.omrilhn.jettipapp.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                topHeader()
+                //topHeader()
+                MainContent()
             }
         }
     }
@@ -49,7 +64,8 @@ fun MyApp(content: @Composable () -> Unit){
 @Composable
 fun topHeader(totalPerPerson : Double = 134.0)
 {//clip: For give a shape to the container
-    Surface(modifier = Modifier.fillMaxWidth()
+    Surface(modifier = Modifier
+        .fillMaxWidth()
         .height(150.dp)
         .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp)))
         ,color = Color(0xFFB160BF)
@@ -66,19 +82,15 @@ fun topHeader(totalPerPerson : Double = 134.0)
         }
     }
 }
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun MainContent()
 {
-    Surface(modifier = Modifier.padding(2.dp)
-        .fillMaxWidth(),shape = RoundedCornerShape(corner = CornerSize(8.dp)),
-        border = BorderStroke(width = 1.dp,color = Color.LightGray)){
-            Column()
-            {
-              
-            }
-
+    BillForm(){billAmount->
+        Log.d("AMT","Main Content: $billAmount")//
     }
+
 }
 
 
@@ -90,5 +102,59 @@ fun GreetingPreview() {
         MyApp {
             Text("Hello again!")
         }
+    }
+}
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun BillForm(modifier : Modifier = Modifier,
+            onValChange : (String) -> Unit = {}
+                 ){
+    val totalBillState = remember{
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value){
+        totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(modifier = Modifier
+        .padding(2.dp)
+        .fillMaxWidth(),shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp,color = Color.LightGray)){
+        Column(modifier = Modifier.padding(6.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start)
+        {
+            InputField(valueState = totalBillState , labelId ="Enter Bill" , enabled = true , isSingleLine = true,
+                onAction = KeyboardActions{
+                    if(!validState) return@KeyboardActions
+                        onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                })
+            if(validState){
+                Row(modifier = Modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start){
+                    Text("Split",
+                        modifier = Modifier.align(
+                            alignment = Alignment.CenterVertically
+                        ))
+                    Spacer(modifier = Modifier.width(120.dp))
+                        Row(modifier = Modifier.padding(horizontal=3.dp),
+                            horizontalArrangement = Arrangement.End
+                            ){  
+                            RoundIconButton( imageVector = Icons.Default.Remove, onClick = { /*TODO*/ })
+
+                            RoundIconButton(
+                                imageVector = Icons.Default.Add
+                                , onClick = { /*TODO*/ })
+
+                        }
+                }
+
+            }else{
+
+            }
+
+        }
+
     }
 }
--- app/src/main/java/com/omrilhn/jettipapp/components/Components.kt
+++ app/src/main/java/com/omrilhn/jettipapp/components/Components.kt
@@ -1,6 +1,7 @@
 package com.omrilhn.jettipapp.components
 
 import android.graphics.drawable.Icon
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
@@ -41,7 +42,7 @@ fun InputField(modifier : Modifier=Modifier,
             singleLine =isSingleLine,
             textStyle = TextStyle(fontSize = 18.sp,
                                     color = MaterialTheme.colorScheme.onBackground),
-        modifier = modifier.padding(bottom =10.dp,start=10.dp,end=10.dp),
+        modifier = modifier.padding(bottom =10.dp,start=10.dp,end=10.dp).fillMaxWidth(),
         enabled = enabled,
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType,
                                             imeAction = imeAction),
